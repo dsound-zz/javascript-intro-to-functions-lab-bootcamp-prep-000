d7ff68202b946e9786c08c7c71af679632fcfc51
demian sims <demiansims@gmail.com>
2018-09-14 20:13:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase() ) {
   return "I can't hear you!"
-}
-else {
-  return "YES INDEED!"
+  
 }
 }
 
