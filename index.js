db1c0472ec514368f3c96cf90c1b6a328cab7fe6
demian sims <demiansims@gmail.com>
2018-09-14 19:49:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 function shout(string) {
+
  return string.toUpperCase()
   }
 function whisper(string) {
+  
   return string.toLowerCase()
     }
 function logShout(string) {
+  
   console.log('HELLO')
 }    
 function logWhisper(string) {
-  console.log('hello')
+  c
+  onsole.log('hello')
 }
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
@@ -22,4 +26,8 @@ function sayHiToGrandma(string) {
    }
      
 } 
+
+
+return string.toUpperCase()
+}
 
